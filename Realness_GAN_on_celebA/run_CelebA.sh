2a7b0475fec1052c62b3165ce8fa2f19641a48f8
HC-0 <harrysbcooper@gmail.com>
2021-05-12 15:22:12 +0100

    first push

---
+++ Realness_GAN_on_celebA/run_CelebA.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+export CUDA_VISIBLE_DEVICES=0;
+
+SEED=1;
+
+mkdir ./LOG 
+
+NAME="RealnessGAN-CelebA256-"$SEED;
+LOG_NAME="./LOG/log_"$NAME".txt";
+#if [ ! -f  $LOG_NAME ]; then
+	OMP_NUM_THREADS=1 python train.py --lr_D .0002 --lr_G .0002 --beta1 .50 --beta2 .999 \
+	--gen_every 10000 --print_every 1000 --G_h_size 32 --D_h_size 32 --gen_extra_images 500 \
+	--input_folder './data/CelebA/Img/' \
+	--image_size 256 \
+	--total_iters 520000 \
+	--seed $SEED \
+	--num_workers 8 \
+	--G_updates  1 \
+	--D_updates  1 \
+	--effective_batch_size   32 \
+	--batch_size   32 \
+	--n_gpu         1  \
+	--positive_skew   1.0 \
+	--negative_skew  -1.0 \
+	--num_outcomes     51 \
+	--use_adaptive_reparam   True \
+	--relativisticG		True \
+	--output_folder "./OUTPUT/"$NAME \
+	--extra_folder  "./OUTPUT/"$NAME"_Extra" \
+	2>&1 | tee $LOG_NAME;
+
+	#	--load_ckpt "./OUTPUT/RealnessGAN-CelebA256-1_Extra/models/state_52.pth" \
+#else
+#	echo "Danger close. The log file at -- $LOG_NAME -- exists!"
+#fi
+
